4a871e2845f01d0f773f76fa4b0686ddde9542d9
Jules <julespouilly0@gmail.com>
2026-02-13 11:52:35 +0100

    debug

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
+var tweetsRouter = require("./routes/tweets");
 var usersRouter = require("./routes/users");
 
 var app = express();
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+app.use("/tweets", tweetsRouter)
 app.use("/users", usersRouter);
 
 module.exports = app;
